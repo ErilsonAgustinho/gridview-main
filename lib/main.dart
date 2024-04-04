f130cd7384deaad2b19308a6b6dc6cc3c131bef1
ErilsonAgustinho <164353156+ErilsonAgustinho@users.noreply.github.com>
2024-04-04 08:29:28 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final title = 'Lista em um Grid';
+    return MaterialApp(
+      title: title,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: GridView.count(
+          // Cria um grid com duas colunas
+          crossAxisCount: 2,
+          // Gera 100 Widgets que exibem o seu índice
+          children: List.generate(100, (index) {
+            return Center(
+                child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [Icon(Icons.person), Text("Pessoa ${index + 1}")],
+            ));
+          }),
+        ),
+      ),
+    );
+  }
+}
